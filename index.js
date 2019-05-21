8f6fced095b43c63da7cba9a4d3132af37c5baad
WilliamHo999 <50350129+WilliamHo999@users.noreply.github.com>
2019-05-22 00:49:46 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+﻿
+<html>
+    <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <title>HelloWorld</title>
+    <script>
+    console.log(‘hello World’);
+    </script>
+    </head>
+<body>
+	<h1>Hello World</h1>
+</body>
+</html>
